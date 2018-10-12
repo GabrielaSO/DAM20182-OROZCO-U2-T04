2e05d372a9c2a64655a6028f08e9a64d29edc823
GabrielaSO <gaitsotoor@ittepic.edu.mx>
2018-10-11 21:31:07 -0600

    cambia pagina 'root' a Page1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,17 +3,20 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
+//import { HomePage } from '../pages/home/home';
+import { Page1 } from '../pages/page1/page1';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  //rootPage:any = HomePage;
+  rootPage:any = Page1;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, 
+    statusBar: StatusBar, 
+    splashScreen: SplashScreen) {
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
